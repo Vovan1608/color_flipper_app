f5da97dec85579aedbb1067ae2917951068803e8
Vovan1608 <vovan1608@gmail.com>
2021-03-27 14:23:17 +0200

    added import\export

--- hex.js
+++ hex.js
@@ -1,14 +1,14 @@
-"use strict";
+import {btn, color, getRandomNumber} from "./helpers/common.js";
 
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+// const btn = document.getElementById("btn");
+// const color = document.querySelector(".color");
 
 function handler () {
 	let hexColor = "#";
 	for (let i = 0; i < 6; i++) {
-		hexColor += hex[getRandomNumber()];
+		hexColor += items[getRandomNumber()];
 	}
 	document.body.style.backgroundColor = hexColor;
 	color.textContent = hexColor;
@@ -16,6 +16,6 @@ function handler () {
 
 btn.addEventListener("click", handler);
 
-function getRandomNumber() {
-	return Math.floor(Math.random() * hex.length);
-}
+// function getRandomNumber() {
+// 	return Math.floor(Math.random() * hex.length);
+// }
--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-"use strict";
+import {btn, color, getRandomNumber} from "./helpers/common.js";
 
-const colors = ["green", "red", "rgb(133, 122, 200)", "#f15025"];
+const items = ["green", "red", "rgb(133, 122, 200)", "#f15025"];
 
-const btn = document.getElementById("btn");
-const color = document.querySelector(".color");
+// const btn = document.getElementById("btn");
+// const color = document.querySelector(".color");
 
 function handler () {
 	const randomNum = getRandomNumber();
-	document.body.style.backgroundColor = colors[randomNum];
-	color.textContent = colors[randomNum];
+	document.body.style.backgroundColor = items[randomNum];
+	color.textContent = items[randomNum];
 }
 
 btn.addEventListener("click", handler);
 
-function getRandomNumber() {
-	return Math.floor(Math.random() * colors.length);
-}
+// function getRandomNumber() {
+// 	return Math.floor(Math.random() * colors.length);
+// }
