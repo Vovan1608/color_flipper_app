dd997906b3e9913cc0afbb125ddf5c9c3475f4a2
Vovan1608 <vovan1608@gmail.com>
2021-03-27 12:58:17 +0200

    added handler, addEventListener, getRandomNumber

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ const colors = ["green", "red", "rgb(133, 122, 200)", "#f15025"];
 
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
+
+function handler () {
+	const randomNum = 2;
+	document.body.style.backgroundColor = colors[randomNum];
+	color.textContent = colors[randomNum];
+}
+
+btn.addEventListener("click", handler);
+
+function getRandomNumber() {
+	return Math.random()
+}
