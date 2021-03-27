0b64155204d0e3dad7bd0f4823d89a90c1e4bfc8
Vovan1608 <vovan1608@gmail.com>
2021-03-27 17:06:34 +0200

    remove modules

--- hex.js
+++ hex.js
@@ -1,9 +1,7 @@
-import {btn, color, getRandomNumber} from "./helpers/common.js";
-
 const items = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-// const btn = document.getElementById("btn");
-// const color = document.querySelector(".color");
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
 
 function handler () {
 	let hexColor = "#";
@@ -16,6 +14,6 @@ function handler () {
 
 btn.addEventListener("click", handler);
 
-// function getRandomNumber() {
-// 	return Math.floor(Math.random() * hex.length);
-// }
+function getRandomNumber() {
+	return Math.floor(Math.random() * items.length);
+}
--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import {btn, color, getRandomNumber} from "./helpers/common.js";
+// import {btn, color, getRandomNumber} from "./helpers/general.js";
 
 const items = ["green", "red", "rgb(133, 122, 200)", "#f15025"];
 
-// const btn = document.getElementById("btn");
-// const color = document.querySelector(".color");
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
 
 function handler () {
 	const randomNum = getRandomNumber();
@@ -13,6 +13,6 @@ function handler () {
 
 btn.addEventListener("click", handler);
 
-// function getRandomNumber() {
-// 	return Math.floor(Math.random() * colors.length);
-// }
+function getRandomNumber() {
+	return Math.floor(Math.random() * items.length);
+}
